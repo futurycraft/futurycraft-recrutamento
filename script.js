93fe7f49bf126d2a7b698ba322133421c16df33f
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-19 16:02:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 // Conexão Supabase
 
 const supabaseClient = window.supabase.createClient(
-    https://jssscxlnzytmwzbabvhu.supabase.co,
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ
+    "https://jssscxlnzytmwzbabvhu.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ"
 );
 
 
