afaaf576017d5e6b8ad2f318f16866dbe7320bf5
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-19 19:01:06 -0300

    Create candidato.js

---
+++ candidato.js
@@ -0,0 +1,186 @@
+const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
+
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
+
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
+
+async function verificarLogin(){
+
+    const {data} = await supabaseClient.auth.getSession();
+
+
+    if(!data.session){
+
+        window.location.href="login.html";
+
+    }
+
+}
+
+
+verificarLogin();
+
+
+
+const parametros = new URLSearchParams(
+    window.location.search
+);
+
+
+const id = parametros.get("id");
+
+
+
+async function carregarCandidato(){
+
+
+const {data,error}=await supabaseClient
+.from("candidatos")
+.select("*")
+.eq("id",id)
+.single();
+
+
+
+if(error){
+
+console.error(error);
+
+return;
+
+}
+
+
+
+document.querySelector("#candidato").innerHTML = `
+
+
+<div class="card-candidato">
+
+
+<h2>
+${data.nick}
+</h2>
+
+
+<p>
+<b>Discord:</b>
+${data.discord}
+</p>
+
+
+<p>
+<b>Idade:</b>
+${data.idade}
+</p>
+
+
+<p>
+<b>Tempo:</b>
+${data.tempo}
+</p>
+
+
+<p>
+<b>Disponibilidade:</b>
+${data.disponibilidade}
+</p>
+
+
+<hr>
+
+
+<h3>Respostas</h3>
+
+
+<p>
+<b>Motivo:</b><br>
+${data.motivo}
+</p>
+
+
+
+<p>
+<b>Como ajudaria:</b><br>
+${data.ajuda}
+</p>
+
+
+
+<p>
+<b>Hack:</b><br>
+${data.hack}
+</p>
+
+
+
+<h3>
+Status:
+${data.status}
+</h3>
+
+
+
+<button onclick="alterarStatus('Aprovado')">
+✅ Aprovar
+</button>
+
+
+
+<button onclick="alterarStatus('Recusado')">
+❌ Recusar
+</button>
+
+
+</div>
+
+
+`;
+
+
+}
+
+
+
+
+async function alterarStatus(status){
+
+
+await supabaseClient
+.from("candidatos")
+.update({
+status:status
+})
+.eq("id",id);
+
+
+
+alert(
+"Candidatura alterada para "+status
+);
+
+
+
+carregarCandidato();
+
+
+}
+
+
+
+function voltar(){
+
+window.location.href="admin.html";
+
+}
+
+
+
+carregarCandidato();
