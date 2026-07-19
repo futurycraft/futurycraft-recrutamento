e3189c9dc741080915192f7754ba1ce281501c9c
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-19 16:30:20 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
 
-const SUPABASE_KEY = "SUA_KEY_AQUI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
 
 
 const supabaseClient = window.supabase.createClient(
