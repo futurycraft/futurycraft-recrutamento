d94cd95070c5e72078ce982f5b42f518f1388a18
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-19 15:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // Conexão Supabase
 
-const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
+const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co/rest/v1/";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
 
