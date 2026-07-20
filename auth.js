54c3e57400e510475eec3201aa9b7bf332a3737f
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-20 03:17:15 -0300

    Update Supabase key in auth.js
    Replaced sensitive Supabase key with a placeholder.

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJqc3NzeGNsbnp5dG13endlYmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
+const SUPABASE_KEY = "SUA_KEY_AQUI";
 
 
 const supabaseClient = window.supabase.createClient(
@@ -16,24 +16,16 @@ async function verificarAcesso(){
     const { data } = await supabaseClient.auth.getSession();
 
 
-
     if(!data.session){
-
 
         window.location.href = "login.html";
 
-
-        return false;
-
+        return;
 
     }
 
 
-    return true;
-
-
 }
-
 
 
 verificarAcesso();
