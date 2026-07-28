cb29c911142d66a349567c99bf0d2a5ef67cabb0
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-28 13:29:45 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -12,9 +12,9 @@
 |
 */
 
-const SUPABASE_URL = "COLE_SUA_SUPABASE_URL_AQUI";
+const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
 
-const SUPABASE_ANON_KEY = "COLE_SUA_SUPABASE_ANON_KEY_AQUI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
 
 
 
