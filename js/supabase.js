3dd4eaf58d735c6e6f35ee2fc0cd4c055b46e60f
mathemaciel <68457323+mathemaciel@users.noreply.github.com>
2026-07-28 13:41:02 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,42 +3,20 @@
    SUPABASE
 ========================================== */
 
-/*
-|--------------------------------------------------------------------------
-| CONFIGURAÇÃO DO SUPABASE
-|--------------------------------------------------------------------------
-|
-| Cole aqui os dados do seu projeto Supabase.
-|
-*/
 
 const SUPABASE_URL = "https://jssscxlnzytmwzbabvhu.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzc3NjeGxuenl0bXd6YmFidmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzE2NzcsImV4cCI6MjEwMDA0NzY3N30.Ku_HJdFQYyEmLmjkynye90l0bpM0MbbFVJPZDMCEOXQ";
+
+const SUPABASE_ANON_KEY = "SUA_KEY_AQUI";
 
 
 
+/* ==========================================
+   CRIA CLIENTE
+========================================== */
 
 
-/*
-|--------------------------------------------------------------------------
-| INICIALIZAÇÃO
-|--------------------------------------------------------------------------
-*/
-
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
-
-
-
-
-
-/*
-|--------------------------------------------------------------------------
-| DISPONIBILIZA GLOBALMENTE
-|--------------------------------------------------------------------------
-*/
-
-window.supabaseClient = supabase;
